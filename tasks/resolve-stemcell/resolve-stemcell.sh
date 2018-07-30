0ceec0aa49e989fd07bde8c658d2b5cc89712b4a
Josh <josh@jtri.com>
2018-07-30 14:40:10 -0600

    updates

--- tasks/resolve-stemcell/resolve-stemcell.sh
+++ tasks/resolve-stemcell/resolve-stemcell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-tile_metadata=$(unzip -l pivnet-product/*.pivotal | grep "metadata" | grep "ml$" | awk '{print $NF}')
+tile_metadata=$(unzip -l p-healthwatch-s3/*.pivotal | grep "metadata" | grep "ml$" | awk '{print $NF}')
 
 STEMCELL_VERSION_FROM_TILE=$(unzip -p p-healthwatch-s3/*.pivotal $tile_metadata | grep -A5 stemcell_criteria:  \
                                   | grep version: | grep -Ei "[0-9]+{2}" | awk '{print $NF}' | sed "s/'//g" )
