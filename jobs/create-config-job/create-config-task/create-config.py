b43606616e5cff5a0d5356d2228c6bec129f5abb
Josh <josh@jtri.com>
2018-07-30 13:30:10 -0600

    updates

--- jobs/create-config-job/create-config-task/create-config.py
+++ jobs/create-config-job/create-config-task/create-config.py
@@ -18,30 +18,19 @@ if tile1_spec =='':
 
 tile1_spec = yaml.load(tile1_spec)
 
+template = tile1_spec['slug'] + "_"+tile1_spec['version']+".yml"
+config_out = tile1_spec['slug'] + ".yml"
 
-print("Let's get it started")
-print("Deployment network is " + tile1_spec['deployment_network'])
+#print("Let's get it started")
+#print("Deployment network is " + tile1_spec['deployment_network'])
 
-# vals = {
-#     'deployment_network': tile1_spec['deployment_network'],
-#     'az1': tile1_spec['az1'],
-#     'az2': tile1_spec['az2'],
-#     'az3': tile1_spec['az3'],
-#     'service_network': tile1_spec['service_network'], 
-#     'opsmgr_url': tile1_spec['opsmgr_url']
-#     }
+print("Slug: " + tile1_spec['slug'])
+print("Version: " + tile1_spec['version'])
+print("Input Template: " + template)
+print("Output Config: " + config_out)
 
-# for key in tile1_spec:
-# 	print(key + " == " + tile1_spec[key])
-
-#with open("template/p-healthwatch-1.yml", "r") as in_file:
-#	with open("configured-template/haas147_p-healthwatch-1.0.0.yml", "w") as out_file:
-#		for line in in_file:
-#			out_file.write(line)
-
-
-with open("pipeline-factory-templates/p-healthwatch_"+tile1_spec['version']+".yml", "r") as in_file:
-	with open("config/p-healthwatch.yml", "w") as out_file:
+with open("pipeline-factory-templates/" + template, "r") as in_file:
+	with open("config/" + config_out, "w") as out_file:
     		t = in_file.read()
     		#rendered = jinja2.Template(t).render(vals)
     		
