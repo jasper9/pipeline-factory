2c400299c9f079418fe8de8757247778cbf7fed0
Josh <josh@jtri.com>
2018-07-30 10:27:26 -0600

    updates

--- jobs/create-config-job/create-config-task/create-config.py
+++ jobs/create-config-job/create-config-task/create-config.py
@@ -1,8 +1,30 @@
+import os,sys
+import yaml
+from pprint import pprint
+import urllib3
+from time import sleep
+import json as json1
+import subprocess
+
+from collections import OrderedDict
+from operator import itemgetter  
+
+tile1_spec            = os.getenv('TILE1_SPEC', '').strip()
 
 
 
+if tile1_spec =='':
+    print('No yaml payload set for TILE1_SPEC')
 
+
+
+tile1_spec = yaml.load(tile1_spec)
+
+
+#slot_spec['main_password']
 
 print("Let's get it started")
+
+print("Deployment network is " + tile1_spec['deployment_network'])
 
 
