fad7e1628475e7d1388297449a092ffb7a594342
Josh <josh@jtri.com>
2018-07-30 10:38:58 -0600

    updates

--- jobs/create-config-job/create-config-task/create-config.py
+++ jobs/create-config-job/create-config-task/create-config.py
@@ -11,20 +11,14 @@ from operator import itemgetter
 
 tile1_spec            = os.getenv('TILE1_SPEC', '').strip()
 
-
-
 if tile1_spec =='':
     print('No yaml payload set for TILE1_SPEC')
-
-
 
 tile1_spec = yaml.load(tile1_spec)
 
 
-#slot_spec['main_password']
-
 print("Let's get it started")
-
 print("Deployment network is " + tile1_spec['deployment_network'])
 
-
+with open("generated-config/config.yml", "w") as f:
+	f.write("blah")
